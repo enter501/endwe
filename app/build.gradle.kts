206090eedba3cbd108648d60233be065774cd48a
enter501 <eastyen@gmail.com>
2026-09-17 19:39:24 +0800

    Bump Endwe version to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "tw.bailudangruo.endwe"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
